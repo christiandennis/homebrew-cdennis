a01080904ab445426d51be33c5f11170c3a8c999
Christian Dennis <christiandennis11@gmail.com>
2016-12-24 20:26:22 -0800

    Update sd.rb

--- sd.rb
+++ sd.rb
@@ -1,7 +1,7 @@
 class Sd < Formula
   desc "Automatically download english subtitle for movies"
   homepage "https://github.com/christiandennis/SubtitleDownloader"
-  url "https://github.com/christiandennis/sd/archive/1.0.0.tar.gz"
+  url "https://github.com/christiandennis/sd/archive/1.0.1.tar.gz"
   version "1.0.0"
   sha256 "7fa55451449a9c5648f5255b6f8c90fe2eb9fd98588179197fbf748e194f6e33"
 
